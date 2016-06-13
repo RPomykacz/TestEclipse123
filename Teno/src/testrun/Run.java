77c2d7873c41514b3a83d797f159163a8e085dd8
RPomykacz <RPomykacz@protonmail.ch>
2016-06-13 21:48:07 +0200

    DAdasda

--- Teno/src/testrun/Run.java
+++ Teno/src/testrun/Run.java
@@ -4,7 +4,9 @@ public class Run {
 
 	public static void main(String[] args) {
 		System.out.println("Nooo");
+		// dad DDdddd
 		System.out.println("Cosi nowego!");
+		
 		System.out.println("Nooooaaaaaa");
 	}
 
